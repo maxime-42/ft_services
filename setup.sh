983db9163474643d72f74176152150331cde3926
maxime-42 <mkayumba@student.42.fr>
2021-01-13 23:59:58 +0100

    pour que rayan teste

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@
 docker image rm -f alpine
 docker image rm -f my_nginx
 docker image rm -f my_mysql
+docker image rm -f my_wordpress
 docker image rm -f my_php_my_admin
-docker image prune
+docker image rm -f my_influxdb
+docker system prune -a
+
 minikube delete
 minikube start --vm-driver=docker
 
@@ -22,17 +25,26 @@ eval $(minikube docker-env) #point the docker client to the machine's docker dae
 docker build --no-cache  -t my_nginx src/nginx/
 docker build --no-cache  -t my_mysql src/mysql/
 docker build --no-cache  -t my_php_my_admin src/php_my_admin/
+docker build --no-cache  -t my_wordpress src/wordpress/
+docker build --no-cache  -t my_influxdb src/influxdb/
+docker build --no-cache  -t my_telegraf src/telegraf/
+docker build --no-cache  -t my_grafana src/grafana/
+# docker build --no-cache  -t my_influxdb src/influxd/Dockerfile
+
 eval $(minikube docker-env -u) #point the docker client to the machine's docker daemon
 
 ################ deployment nginx #############
 
 kubectl apply -f src/nginx/deployment_nginx.yaml
-kubectl apply -f src/mysql/deployement_mysql.yaml
-kubectl apply -f src/php_my_admin/deployement_php.yaml
+kubectl apply -f src/mysql/deployment_mysql.yaml
+kubectl apply -f src/php_my_admin/deployment_php.yaml
+kubectl apply -f src/wordpress/deployment_wordpress.yaml
+kubectl apply -f src/influxdb/deployment_influxdb.yaml
+kubectl apply -f src/telegraf/deployment_telegraf.yaml 
+kubectl apply -f src/grafana/deployment_grafana.yaml
+# kubectl apply -f src/influxd/deployment_influxdb.yaml
 
 # docker run -tid  -p 80:80 -p 443:443 --name test1 my_nginx
 #  kubectl get all --watch
-
-########### mysql ####################
 
 minikube dashboard
--- src/mysql/setup_mysql.sh
+++ src/mysql/setup_mysql.sh
@@ -5,7 +5,8 @@ mysql_install_db --datadir=/var/lib/mysql
 rc-service mariadb restart
 #### creat data base  ####
 mysql -u root mysql < ./creatdb.sql
-# mysql -u root wordpress < ./wordpress.sql
+# rc-service mariadb restart
+mysql -u root wordpress < ./wordpress.sql
 
 rc-service mariadb stop
 mysqld --user=root
