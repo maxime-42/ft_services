d16ffeaa2406617b279c747e9505fc754c34d559
maxime-42 <mkayumba@student.42.fr>
2021-01-15 02:01:21 +0100

    modification de src a srcs

--- setup.sh
+++ setup.sh
@@ -30,33 +30,33 @@ minikube start --vm-driver=docker
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f src/metallb/metallb_conf.yaml
+kubectl apply -f srcs/metallb/metallb_conf.yaml
 
 
 
 ######################## 			build image 				##################################################
 
 eval $(minikube docker-env) 		#point the docker client to the machine's docker daemon
-docker build --no-cache  -t my_nginx src/nginx/
-docker build --no-cache  -t my_mysql src/mysql/
-docker build --no-cache  -t my_php_my_admin src/php_my_admin/
-docker build --no-cache  -t my_wordpress src/wordpress/
-docker build --no-cache  -t my_influxdb src/influxdb/
-docker build --no-cache  -t my_telegraf src/telegraf/
-docker build --no-cache  -t my_grafana src/grafana/
-docker build --no-cache  -t my_ftps src/ftps/
+docker build --no-cache  -t my_nginx srcs/nginx/
+docker build --no-cache  -t my_mysql srcs/mysql/
+docker build --no-cache  -t my_php_my_admin srcs/php_my_admin/
+docker build --no-cache  -t my_wordpress srcs/wordpress/
+docker build --no-cache  -t my_influxdb srcs/influxdb/
+docker build --no-cache  -t my_telegraf srcs/telegraf/
+docker build --no-cache  -t my_grafana srcs/grafana/
+docker build --no-cache  -t my_ftps srcs/ftps/
 eval $(minikube docker-env -u) 		#point the docker client to the machine's docker daemon	
 
 #######################				deployment 					##################################################
 
-kubectl apply -f src/nginx/deployment_nginx.yaml
-kubectl apply -f src/mysql/deployment_mysql.yaml
-kubectl apply -f src/php_my_admin/deployment_php.yaml
-kubectl apply -f src/wordpress/deployment_wordpress.yaml
-kubectl apply -f src/influxdb/deployment_influxdb.yaml
-kubectl apply -f src/telegraf/deployment_telegraf.yaml 
-kubectl apply -f src/grafana/deployment_grafana.yaml
-kubectl apply -f src/ftps/deployment_ftps.yaml
+kubectl apply -f srcs/nginx/deployment_nginx.yaml
+kubectl apply -f srcs/mysql/deployment_mysql.yaml
+kubectl apply -f srcs/php_my_admin/deployment_php.yaml
+kubectl apply -f srcs/wordpress/deployment_wordpress.yaml
+kubectl apply -f srcs/influxdb/deployment_influxdb.yaml
+kubectl apply -f srcs/telegraf/deployment_telegraf.yaml 
+kubectl apply -f srcs/grafana/deployment_grafana.yaml
+kubectl apply -f srcs/ftps/deployment_ftps.yaml
 
 minikube dashboard
 echo -ne 	"\n\n\033[1;36m
