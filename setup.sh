af6145199d51789d516b79a044021602453dbea6
maxime-42 <mkayumba@student.42.fr>
2021-01-16 12:40:06 +0100

    update

--- setup.sh
+++ setup.sh
@@ -18,12 +18,12 @@ minikube start --vm-driver=docker
 
 ######################## 			for vm 						##################################################
 
-CLUSTER_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
-sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/metallb/metallb_conf.yaml
-sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/ftps/setup_ftps.sh
-sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/nginx/nginx.conf
-sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/mysql/wordpress.sql
-sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/telegraf/telegraf.conf
+#CLUSTER_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
+#sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/metallb/metallb_config.yaml
+#sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/ftps/start.sh
+#sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/nginx/nginx.conf
+#sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/mysql/wordpress.sql
+#sed -i 's/192.168.49.2/'$CLUSTER_IP'/g' srcs/telegraf/telegraf.conf
 
 ######################## 			stuff metallb				##################################################
 
